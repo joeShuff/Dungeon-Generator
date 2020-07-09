8b7acf1720730f6d1406781fd710bcd1937fc916
Joe Shufflebotham <joe.shufflebotham13@gmail.com>
2020-07-09 16:50:01 +0100

    RoomFeature translated

---
+++ app/src/main/java/com/joeshuff/dddungeongenerator/generator/features/RoomFeature.kt
@@ -0,0 +1,7 @@
+package com.joeshuff.dddungeongenerator.generator.features
+
+interface RoomFeature {
+    val pageForMoreInfo: Int
+    val featureName: String?
+    val featureDescription: String?
+}
